628465995ebed7047ed19d3d43d8816edeebbfa1
wllcstr <48567254+wllcstr@users.noreply.github.com>
2020-03-14 23:37:53 -0300

    [ENH] Atualizando Front-End

---
+++ TaskList/TaskList/js/init.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    var loginData = {
+        grant_type: 'password',
+        username: "wllcstr",
+        password: "q1w2e3r4"
+    };
+
+    
+
+    if(!sessionStorage.apiurl) {
+        baseurl =  prompt("Por favor, preencha a URL Base da API.");
+        sessionStorage.setItem("apiurl", baseurl);
+        location.reload();
+    }
+
+    
+    $.ajax({
+        type: 'POST',
+        url: sessionStorage.apiurl + '/token',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: loginData
+       
+    }).done(function (data) {
+        sessionStorage.setItem("token", data.access_token);
+        }).fail(function (a, b, c) {
+            var jqXHR = a;
+            var textStatus = b;
+            var errorThrown = c;
+
+            
+            alert("deu merda");
+        });
+});
---
+++ TaskList/TaskList/obj/Release/Package/PackageTmp/js/init.js
@@ -0,0 +1,33 @@
+﻿
+
+
+$(document).ready(function () {
+    var loginData = {
+        grant_type: 'password',
+        username: "wllcstr",
+        password: "q1w2e3r4"
+    };
+
+    alert(loginData.username);
+
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://superotasklist.azurewebsites.net/token',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: 'grant_type=password&username=wllcstr&password=q1w2e3r4'
+       
+    }).done(function (data) {
+        alert("deu token porra");
+        sessionStorage.setItem("token", data.access_token);
+        }).fail(function (a, b, c) {
+            var jqXHR = a;
+            var textStatus = b;
+            var errorThrown = c;
+
+            
+            alert("deu merda");
+        });
+});
